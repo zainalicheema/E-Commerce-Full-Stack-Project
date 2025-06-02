4b8a5862e5149b9a0bd16dcf2c0d2b45feed873a
Zain Ali Cheema <zainalicheema9@gmail.com>
2025-06-02 04:36:42 -0700

    Remove ENV url

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://fooddelivery:fooddelivery123@cluster0.tze40.mongodb.net/food-del')
+    await mongoose.connect('Enter Your MongoB URL')
     .then(()=>console.log("DB Connected"));
 }
